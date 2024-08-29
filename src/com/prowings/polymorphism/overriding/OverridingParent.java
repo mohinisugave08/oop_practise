779fccf0996f5cb2fd8808bed165fa012a2a6011
mohinisugave08 <mohinisugave08@gmail.com>
2024-08-29 15:19:51 +0530

    updated OverridingParent class

--- src/com/prowings/polymorphism/overriding/OverridingParent.java
+++ src/com/prowings/polymorphism/overriding/OverridingParent.java
@@ -3,12 +3,12 @@ package com.prowings.polymorphism.overriding;
 public class OverridingParent {
 
     public static void m1() {
-    	System.out.println("Inside parent - m1()");
+    	System.out.println("Inside parent class method - m1()");
 		
 	}
 	public Number m2() {
 		
-		System.out.println("Inside parent - m2()");
+		System.out.println("Inside parent class method - m2()");
 		return 2;
 		
 	}
